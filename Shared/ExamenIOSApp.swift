441b80bc9f815591b6c9de6110cfd90057263e99
JesúsFranciscoLeyva <leyva527@gmail.com>
2022-07-21 18:16:14 -0600

    Initial Commit

---
+++ Shared/ExamenIOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  ExamenIOSApp.swift
+//  Shared
+//
+//  Created by Jesús Francisco Leyva Juárez on 21/07/22.
+//
+
+import SwiftUI
+
+@main
+struct ExamenIOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
