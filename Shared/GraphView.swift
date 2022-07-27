e7354abfbc6a40c4ede9bbca34a4ef67e05736ba
JesúsFranciscoLeyva <leyva527@gmail.com>
2022-07-26 19:22:25 -0600

    Graph view

---
+++ Shared/GraphView.swift
@@ -0,0 +1,20 @@
+//
+//  GraphView.swift
+//  ExamenIOS (iOS)
+//
+//  Created by Jesús Francisco Leyva Juárez on 21/07/22.
+//
+
+import SwiftUI
+
+struct GraphView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct GraphView_Previews: PreviewProvider {
+    static var previews: some View {
+        GraphView()
+    }
+}
