35521e043926af4a39a022a3811a43c2ee032f40
JesúsFranciscoLeyva <leyva527@gmail.com>
2022-07-26 19:21:42 -0600

    Examen IOS

--- Shared/ExamenIOSApp.swift
+++ Shared/ExamenIOSApp.swift
@@ -6,12 +6,17 @@
 //
 
 import SwiftUI
+import Firebase
 
 @main
 struct ExamenIOSApp: App {
+    init() {
+        FirebaseApp.configure()
+    }
     var body: some Scene {
         WindowGroup {
             principalView()
+           // ContentView()
         }
     }
 }
